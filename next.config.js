b57302bef4fbfe6d99411f0a3c9d816fc8237df9
shimatsuoffice <shimatsuoffice@gmail.com>
2025-08-10 08:46:49 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true,
-  },
-};
+    unoptimized: true
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
